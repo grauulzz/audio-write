4ebe7e82f18e6bbd9a054d37b6677838dc7531e8
grauulzz <grauulzz@gmail.com>
2022-02-01 12:47:10 -0600

    scala-template

---
+++ src/main/scala/org/grauulzz/playground/Main.scala
@@ -0,0 +1,31 @@
+package org.grauulzz
+package playground
+
+@main def Main(args: String*): Unit =
+    println(Console.GREEN)
+
+        object Calculator:
+        // instance variable
+        private var _calculatorsCreated: Int = 0
+        // getter
+        def calculatorsCreated: Int = _calculatorsCreated
+        // setter
+        private def calculatorsCreated_=(newValue: Int) =
+        _calculatorsCreated = newValue
+        
+    class Calculator(a: Int = 0):
+        // counter for how many instances created
+        Calculator.calculatorsCreated += 1
+        def add(b: Int): Int =
+        a + b
+        def sub(b: Int): Int = 
+        a - b  
+
+    val c1 = Calculator(1)
+    val c2 = Calculator(1)
+    
+    println(c1.sub(1))
+
+    println(Calculator.calculatorsCreated)    
+    
+    println(Console.RESET)
---
+++ src/test/scala/org/grauulzz/playground/ExampleSuite.scala
@@ -0,0 +1,7 @@
+package org.grauulzz
+package playground
+
+final class ExampleSuite extends TestSuite:
+  test("hello world") {
+    1 `shouldBe` 1
+  }
